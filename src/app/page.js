14c7c2f90e298e6959df96d39a5a92f48309cdc6
nishchal <singhnishchal27@gmail.com>
2024-01-31 16:16:57 +0530

    updates

--- src/app/page.js
+++ src/app/page.js
@@ -8,7 +8,7 @@ export default function Home() {
 
   return (
     <div className="w-auto">
-      <div className="px-4 sm:px-14 flex flex-col items-center py-2 my-4 min-h-screen justify-center text-center">
+      <div className="px-4 sm:px-16 flex flex-col items-center py-2 my-4 min-h-screen justify-center text-center">
         <h1 className="text-4xl font-bold mb-4">
           Collatz Conjecture Visualizer
         </h1>
